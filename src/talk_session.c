f5b004ba0d414c19ea7646ce22f3c67414a5126c
Roma Tsutsumi <romat.dev@gmail.com>
2024-06-17 11:47:12 +0900

    feat: use pthread for talk session to reduce delay

--- src/talk_session.c
+++ src/talk_session.c
@@ -1,38 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
-void talk_session(int s){
-    int N = 1024;
+typedef struct thread_arg { 
+    int s;
+} TALK_THREAD_ARG;
 
-    FILE *fp_rec, *fp_play;
+void *rec_send_thread(void *arg) {
+    TALK_THREAD_ARG *thread_arg = (TALK_THREAD_ARG *)arg;
+    int s = thread_arg->s;
+
+    FILE *fp_rec;
     char *cmd_rec = "rec -t raw -b 16 -c 1 -e s -r 44100 - ";
-    char *cmd_play = "play -t raw -b 16 -c 1 -e s -r 44100 - ";
-
     fp_rec = popen(cmd_rec, "r");
-    fp_play = popen(cmd_play, "w");
-    if(fp_rec == NULL || fp_play == NULL){
+    if(fp_rec == NULL){
         perror("popen");
         exit(EXIT_FAILURE);
     }
 
+    int N = 1024;
     unsigned char buffer_rec[N];
-    unsigned char buffer_play[N];
     while(1){
         int n = fread(buffer_rec, 1, N, fp_rec);
         if(n == -1){
-            perror("read"); 
+            perror("read");
             exit(EXIT_FAILURE);
+        }
+        if(n == 0){
+            break;
         }
         n = send(s, buffer_rec, n, 0);
         if(n == -1){
             perror("send"); 
             exit(EXIT_FAILURE);
         }
-        n = recv(s, buffer_play, N, 0);
+    }
+    fclose(fp_rec);
+}
+
+void *recv_play_thread(void *arg) {
+    TALK_THREAD_ARG *thread_arg = (TALK_THREAD_ARG *)arg;
+    int s = thread_arg->s;
+
+    FILE *fp_rec, *fp_play;
+    char *cmd_play = "play -t raw -b 16 -c 1 -e s -r 44100 - ";
+    fp_play = popen(cmd_play, "w");
+    if(fp_play == NULL){
+        perror("popen");
+        exit(EXIT_FAILURE);
+    }
+
+    int N = 1024;
+    unsigned char buffer_play[N];
+    while(1){
+        int n = recv(s, buffer_play, N, 0);
         if(n == -1){
             perror("recv");
             exit(EXIT_FAILURE);
+        }
+        if(n == 0){
+            break;
         }
         n = fwrite(buffer_play, 1, n, fp_play);
         if(n == -1){
@@ -40,6 +68,25 @@ void talk_session(int s){
             exit(EXIT_FAILURE);
         }
     }
-    fclose(fp_rec);
     fclose(fp_play);
+}
+
+
+void talk_session(int s){
+    pthread_t thread_rec_send, thread_recv_play;
+
+    TALK_THREAD_ARG thread_arg = {s};
+
+    if(pthread_create(&thread_rec_send, NULL, rec_send_thread, (void *)&thread_arg) != 0) {
+        printf("Failed to create rec_send_thread.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pthread_create(&thread_recv_play, NULL, recv_play_thread, (void *)&thread_arg) != 0) {
+        printf("Failed to create recv_play_thread.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_join(thread_rec_send, NULL);
+    pthread_join(thread_recv_play, NULL);
 }
