f000cd9e478b0d352a444e15f0bd43b2e6295a29
romasw <70875233+romasw@users.noreply.github.com>
2024-06-18 13:12:37 +0900

    feat: add music when calling and getting a call (#1)
    * feat: add yobidasi
    
    * feat: implement ringtone

--- src/call_thread.c
+++ src/call_thread.c
@@ -1,15 +1,23 @@
 #include "thread_type.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <wait.h>
 
 void *call_thread(void *arg) {
     THREAD_ARG *thread_arg = (THREAD_ARG *)arg;
     int s = thread_arg->s;
     int *flag = thread_arg->flag;
-    
+
+    FILE *fp_play;
+    char *cmd_play = "play ./audio/yobidasi.wav repeat 1";
+    pid_t pid;
+
     while(1){
         if(*flag == 0){
             printf("電話を開始しますか？\nYES\n");
@@ -23,8 +31,22 @@ void *call_thread(void *arg) {
             }
             *flag = 1;
             send(s, data, 5, 0);
-            while (*flag != 4) {
-                usleep(200*1000);
+            pid = fork();
+            if (pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if (pid == 0) {
+                // 子プロセス: sox playコマンドを実行
+                execlp("play", "play", "-q", "./audio/yobidasi.wav", "repeat", "10", (char *)NULL);
+                perror("execlp");
+                exit(EXIT_FAILURE);
+            } else {
+                while (*flag != 4) {
+                    usleep(200*1000);
+                }
+                kill(pid, SIGTERM); // 子プロセスを停止
+                wait(NULL);
             }
             *flag = 1;
             if(!strcmp(thread_arg->input, "rejected")){ //rejectされた
--- src/wait_thread.c
+++ src/wait_thread.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <signal.h>
+#include <wait.h>
 
 void *wait_thread(void *arg) {
     THREAD_ARG *thread_arg = (THREAD_ARG *)arg;
@@ -14,6 +16,9 @@ void *wait_thread(void *arg) {
     char buffer[10];
     char sample_yes[9] = "accepted";
     char sample_no[9] = "rejected";
+
+    pid_t pid;
+
     while(1){
         if(*flag == 0){
             n = recv(s, buffer, 10, 0);
@@ -25,8 +30,22 @@ void *wait_thread(void *arg) {
             if(!strcmp(buffer, "call")){// wait for the call
                 printf("Would you accept this call?\n");
                 *flag = 2;
-                while(*flag != 4){
-                    usleep(200*1000);
+                pid = fork();
+                if (pid == -1) {
+                    perror("fork");
+                    exit(EXIT_FAILURE);
+                }
+                if (pid == 0) {
+                    // 子プロセス: sox playコマンドを実行
+                    execlp("play", "play", "-q", "./audio/chakusin.wav", "repeat", "10", (char *)NULL);
+                    perror("execlp");
+                    exit(EXIT_FAILURE);
+                } else {
+                    while (*flag != 4) {
+                        usleep(200*1000);
+                    }
+                    kill(pid, SIGTERM); // 子プロセスを停止
+                    wait(NULL);
                 }
                 *flag = 2;
                 if(!strcmp(thread_arg->input, "yes")){
